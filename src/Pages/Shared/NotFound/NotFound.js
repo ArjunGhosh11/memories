7dc82cd5121081365c5b0f49b19cdfb97407561c
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-04-17 10:24:04 +0600

    Bunch of components added

---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NotFound.css';
+const NotFound = () => {
+    return (
+        <div>
+            <h1>This is Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
