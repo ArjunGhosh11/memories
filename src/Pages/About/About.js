2140b05adc0edddd90b00c9a478b3228d0277cbd
ArjunGhosh11 <ghosh.rj11@gmail.com>
2022-04-17 18:09:01 +0600

    Blogs and About Added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './About.css';
+import pic from '../../Images/pic.jpg'
 const About = () => {
     return (
-        <div>
-            <h2>This is about</h2>
+        <div className='about'>
+            <h2 className='text-center fw-bolder'>ABOUT ME</h2>
+            <img src={pic} alt="" />
+            <p>Hello everyone!!
+                My name is Arjun Ghosh. Currently I am doing bachelors in Computer Science and Engineering. It has been quite hectic for me since I was doing this course along side my university work and I could not afford to lose my scholarship. Still I worked through it and am quite proud of myself for it.
+                Now my next  'MILESTONE' is to explore machine learning and I am also planning to do some programming competition. I want to explore one thing at a time, so that I don't mess up everything.
+            </p>
         </div>
     );
 };
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './Blogs.css';
 const Blogs = () => {
     return (
-        <div>
-            <h2>This is Blogs</h2>
+        <div className='answers'>
+            <h1 className='text-center'>Question & Answers</h1>
+            <h4>1)Difference between authorization and authentication</h4>
+            <p>= </p>
+            <h4>2)Why are you using firebase? What other options do you have to implement authentication?</h4>
+            <p>= </p>
+            <h4>3)What other services does firebase provide other than authentication?</h4>
+            <p>=</p>
+
         </div>
     );
 };
